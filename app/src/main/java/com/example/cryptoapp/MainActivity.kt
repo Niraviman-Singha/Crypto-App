854ed3ddfec46ddc26d748786c3bb744080f69b4
Niraviman-Singha <niraviman15-11689@diu.edu.bd>
2024-11-19 00:07:25 +0600

    setting up a RecyclerView with View Binding and a custom adapter in an Android activity.

--- app/src/main/java/com/example/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.cryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cryptoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var rvAdapter: RvAdapter
+    private lateinit var data:ArrayList<Model>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        data = ArrayList<Model>()
+        rvAdapter = RvAdapter(this, data)
+
+        binding.rv.layoutManager = LinearLayoutManager(this)
+        binding.rv.adapter = rvAdapter
     }
 }
